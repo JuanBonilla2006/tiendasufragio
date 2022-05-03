cf5684038fdf1a6b27a3e175bbce216ef70f8fa9
13 <13@gmail.com>
2022-05-03 15:50:13 -0500

    johnber

---
+++ firebase.js
@@ -0,0 +1,19 @@
+ /*
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBzof_YDPLjtlAPapA_R7ioS9oKUkNPA-E",
+    authDomain: "tienda-3a0de.firebaseapp.com",
+    projectId: "tienda-3a0de",
+    storageBucket: "tienda-3a0de.appspot.com",
+    messagingSenderId: "686087631666",
+    appId: "1:686087631666:web:f2bbc449764f23a4031d2b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+*/
