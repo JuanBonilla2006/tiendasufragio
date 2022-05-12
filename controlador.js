ab6c46fca0ab0ffec6126c3ff1335258eb9af4b0
you <you@gmail.com>
2022-05-12 17:13:57 -0500

    a

--- controlador.js
+++ controlador.js
@@ -1,48 +1,49 @@
 //OBJETOS
 let producto1={
-    nombre:"ps5",
+    nombre:"Ps5",
     precio:500000,
     estado:"Nuevo",
+    popularidad:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/800px-Estrella_amarilla.png?alt=media&token=cd6deca5-293b-461f-817a-fee3267847f2",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Ps5.jpg?alt=media&token=32edadb2-6888-4f3b-b2fe-8c6bdd357fe5"
 }
 
 let producto2={
-    nombre:"ps4",
+    nombre:"Ps4",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Ps4.jpg?alt=media&token=5c1a3195-a391-4ec1-a008-7e6950d95f86"
 }
 
 let producto3={
-    nombre:"ps5 control",
+    nombre:"Ps5 control",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/PS5CONTROL.jpg?alt=media&token=e3496867-4e06-4773-9586-db84720bb00c"
 }
 
 let producto4={
-    nombre:"ps4 control",
+    nombre:"Ps4 control",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ps4Control.jpg?alt=media&token=1e28191b-656c-41ca-943f-46e4826dbc22"
 }
 
 let producto5={
-    nombre:"the last of us 2",
+    nombre:"The Last Of Us 2",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/TLOU.jpg?alt=media&token=29fdb5fe-031e-4d82-abef-f8cc6ff02cac"
 }
 
 let producto6={
-    nombre:"Forbiden West",
+    nombre:"Horizon Forbiden West",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ForbidenWest.jpg?alt=media&token=f1d4a82d-9947-4c11-a08b-c3ca19c6d950"
 }
 
 let producto7={
-    nombre:"Xbox one x",
+    nombre:"Xbox One x",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/XboxX.jpg?alt=media&token=506aa115-883f-478f-b30d-374731201a08"
@@ -63,7 +64,7 @@ let producto9={
 }
 
 let producto10={
-    nombre:"XboxHeadset",
+    nombre:"Xbox Headset",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/XboxHeadset.jpg?alt=media&token=f3d40f53-2868-4ab3-99fe-6726a3b3c48b"
@@ -77,21 +78,21 @@ let producto11={
 }
 
 let producto12={
-    nombre:"Horizon5",
+    nombre:"Forza Horizon 5",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Horizon5.jpg?alt=media&token=47c656ce-c663-4d45-993d-31259c494551"
 }
 
 let producto13={
-    nombre:"haloInfinito",
+    nombre:"Halo Infinite",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/HaloInfinite.jpg?alt=media&token=dd9aaae8-e4c1-41e7-9cdb-ed0874a5171d"
 }
 
 let producto14={
-    nombre:"Gears",
+    nombre:"Gears 5",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Gears5.jpg?alt=media&token=632aca1b-ef98-4815-9edc-d1d6fe44744d"
@@ -133,7 +134,198 @@ let producto20={
     estado:"Nuevo",
     foto:""
 }
+let producto21={
+    nombre:"Ultimate Controller",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
 
+let producto22={
+    nombre:"Forza Horizon 5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto23={
+    nombre:"Halo Infinite",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto24={
+    nombre:"Gears 5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto25={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto26={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto27={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto28={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto29={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto30={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto31={
+    nombre:"Ultimate Controller",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto32={
+    nombre:"Forza Horizon 5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto33={
+    nombre:"Halo Infinite",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto34={
+    nombre:"Gears 5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto35={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto36={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto37={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto38={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto39={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto40={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto41={
+    nombre:"Ultimate Controller",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto42={
+    nombre:"Forza Horizon 5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto43={
+    nombre:"Halo Infinite",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto44={
+    nombre:"Gears 5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto45={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto46={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto47={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:"s"
+}
+let producto48={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto49={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+let producto50={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
 //ARREGLO
 let productos=Array(
     producto1,
@@ -155,7 +347,37 @@ let productos=Array(
     producto17,
     producto18,
     producto19,
-    producto20
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
 )
 
 //RECORRO EL ARREGLO
@@ -165,14 +387,37 @@ productos.forEach(function(producto){
     let foto=document.createElement("img")
     foto.classList.add("w-100","img-fluid")
     foto.src=producto.foto
+
     let columna=document.createElement("div")
     columna.classList.add("col")
+
     let tarjeta=document.createElement("div")
     tarjeta.classList.add("card")
+
+    let nombreProducto=document.createElement("h5")
+    nombreProducto.classList.add("text-center")
+    nombreProducto.textContent=producto.nombre
+
+    let precioProducto=document.createElement("p")
+    precioProducto.classList.add("text-center")
+    precioProducto.textContent="Precio:"+" "+"$"+producto.precio
+
+    let estadoProducto=document.createElement("p")
+    estadoProducto.classList.add("text-center")
+    estadoProducto.textContent="Estado:"+" "+producto.estado
+
+    let popularidadProducto=document.createElement("p")
+    popularidadProducto.classList.add("text-center")
+    popularidadProducto.textContent="Popularidad"+" "+producto.popularidad
+
+    
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    tarjeta.appendChild(estadoProducto)
+    tarjeta.appendChild(popularidadProducto)
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
-
 
 
 
