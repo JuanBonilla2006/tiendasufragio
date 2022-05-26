38a5bee1c9ab5208534d1226e6b0cb26d69ceff9
you <you@gmail.com>
2022-05-25 19:20:54 -0500

    a

--- controlador.js
+++ controlador.js
@@ -1,378 +1,428 @@
 //OBJETOS
 let producto1={
     nombre:"Dark Souls Remasterd",
-    precio:500000,
+    precio:112000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/DS1.jpg?alt=media&token=478e4641-6e61-45bd-8ab7-afa4f1e5defd"
 }
 
 let producto2={
     nombre:"Dark Souls 2",
-    precio:500000,
+    precio:93500,
     estado:"Nuevo",
     popularidad:"★★★☆☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/DS2.jpg?alt=media&token=ab5808f1-5cbf-44e7-b292-b34c55e4fe0f"
 }
 
 let producto3={
     nombre:"Dark Souls 3",
-    precio:500000,
+    precio:124000,
     estado:"Nuevo",
     popularidad:"★★★★☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/DS3.jpg?alt=media&token=d8fe5776-8bae-41f9-9759-d16514cc06a5"
 }
 
 let producto4={
     nombre:"Sekiro Shadow Die Twice",
-    precio:500000,
+    precio:132000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Sekiro.jpg?alt=media&token=b2fbb30d-0afa-479e-b756-5820dda21acf"
 }
 
 let producto5={
     nombre:"Elden Ring",
-    precio:500000,
+    precio:205000,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/EldenRing.jpg?alt=media&token=e526d7f2-7707-4b30-9f49-37649e294934"
 }
 
 let producto6={
     nombre:"Bioshock",
-    precio:500000,
+    precio:62000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BioShock.jpg?alt=media&token=132113b1-692e-48ee-aac2-a78964b04f76"
 }
 
 let producto7={
     nombre:"Bioshock 2",
-    precio:500000,
+    precio:65000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BioShock2.jpg?alt=media&token=af0dcd92-ebd8-4286-9e37-7c01876a46d4"
 }
 
 let producto8={
     nombre:"Bioshock Infinte",
-    precio:500000,
+    precio:61000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BioShockInfinite.jpg?alt=media&token=ca6e0789-5339-4866-b7b6-6a685439d322"
 }
 
 let producto9={
     nombre:"FIFA 20",
-    precio:500000,
+    precio:106500,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Fifa20.jpg?alt=media&token=d0778157-c9dd-43b9-b7c3-655e35148740"
 }
 
 let producto10={
     nombre:"FIFA 21",
-    precio:500000,
+    precio:148000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Fifa21.jpg?alt=media&token=52304c97-7b34-43d6-becd-7cbdf97f1f6d"
 }
 
 let producto11={
     nombre:"FIFA 22",
-    precio:500000,
+    precio:194000,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Fifa22.jpg?alt=media&token=7e194efe-cd17-4f3f-8b66-39ddcdce6682"
 }
 
 let producto12={
     nombre:"GTA 3",
-    precio:500000,
+    precio:32000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★☆☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/GTA3.jpg?alt=media&token=d0bdd79c-958c-497f-93f0-f751e35e755c"
 }
 
 let producto13={
     nombre:"GTA 4",
-    precio:500000,
+    precio:43900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/GTA4.jpg?alt=media&token=dfc6d4dc-8d9e-4d73-b76d-afcad6a3aaae"
 }
 
 let producto14={
     nombre:"GTA 5",
-    precio:500000,
+    precio:89900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/GTA5.jpg?alt=media&token=6f472132-af9c-44b3-a53b-30e1b96ec850"
 }
 
 let producto15={
     nombre:"Halo 4",
-    precio:500000,
+    precio:62000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★☆☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Halo4.jpg?alt=media&token=961eb2ca-7be3-46ba-a256-546943c897ca"
 }
 let producto16={
     nombre:"Halo 5 Guardians",
-    precio:500000,
+    precio:116000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Halo5.jpg?alt=media&token=95ce1083-f3a9-4097-b433-aa831c27e19b"
 }
 let producto17={
     nombre:"Halo Infinite",
-    precio:500000,
+    precio:178000,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/HaloInfinte.jpg?alt=media&token=4fa5a409-23b8-410c-a945-f287bd239c4b"
 }
 let producto18={
     nombre:"The Elder Scroll III: Morrowind",
-    precio:500000,
+    precio:31900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Morrowind.jpg?alt=media&token=073afefe-551f-4d6f-99dc-de033bc24be0"
 }
 let producto19={
     nombre:"The Elder Scroll IV: Oblivion",
-    precio:500000,
+    precio:48000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Oblivion..jpg?alt=media&token=5650f2e7-7128-4a7d-a66c-612b504407c9"
 }
 let producto20={
     nombre:"The Elder Scroll V: Skyrim",
-    precio:500000,
+    precio:96900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Skyrim.jpg?alt=media&token=efc6a6f8-f598-4528-9201-1665b9235a6a"
 }
 let producto21={
     nombre:"Watch Dogs",
-    precio:500000,
+    precio:78000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★☆☆☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/WatchDogs.jpg?alt=media&token=905f1a2b-5f50-4992-9757-3de4e471683e"
 }
 
 let producto22={
     nombre:"Watch Dogs 2",
-    precio:500000,
+    precio:112000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/WatchDogs2.jpg?alt=media&token=7543f9c4-ee1f-4c4f-baa9-b23abaa2e0b1"
 }
 
 let producto23={
     nombre:"Watch Dogs Legion",
-    precio:500000,
+    precio:152000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/WDLegion.jpg?alt=media&token=31a179f2-c6f0-4fcc-ab8b-a3050d433dd3"
 }
 
 let producto24={
     nombre:"Just Cause 2",
-    precio:500000,
+    precio:22000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★☆☆☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/JustCause2.jpg?alt=media&token=fec76e32-9bbe-4be6-8b28-ab483c9958ee"
 }
 
 let producto25={
     nombre:"Just Cause 3",
-    precio:500000,
+    precio:48000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/JustCause3.jpg?alt=media&token=62cbbc01-c259-4c95-a249-0abcbdf66a9f"
 }
 let producto26={
     nombre:"Just Cause 4",
-    precio:500000,
+    precio:89000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/JustCause4.jpg?alt=media&token=985bbc09-4ae5-4fe6-9188-5b8b14f5fc6e"
 }
 let producto27={
     nombre:"Gears Of War 3",
-    precio:500000,
+    precio:31000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Gears3.jpg?alt=media&token=b67e41e9-f061-45ce-99bb-35ca7ce5efc1"
 }
 let producto28={
     nombre:"Gears Of War 4",
-    precio:500000,
+    precio:94000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Gears4.jpg?alt=media&token=c48f51c1-ebf8-46d1-b63e-7e917cf1b52e"
 }
 let producto29={
     nombre:"Gears Of War 5",
-    precio:500000,
+    precio:143900,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Gears5.jpg?alt=media&token=db49680a-63d1-4556-955a-1f2361a9095b"
 }
 let producto30={
     nombre:"Dragon Ball Xenoverse",
-    precio:500000,
+    precio:49900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Xenoverse.jpg?alt=media&token=82a627ef-fba3-4014-a444-4a32c7f2bbc3"
 }
 let producto31={
     nombre:"Dragon Ball Xenoverse 2",
-    precio:500000,
+    precio:121900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Japón",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Xenoverse2.jpg?alt=media&token=76c07a1e-6f57-438f-86c3-f3fa39803a15"
 }
 
 let producto32={
     nombre:"Forza Horizon 1",
-    precio:500000,
+    precio:32500,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★☆☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ForzaHorizon1.jpg?alt=media&token=10f04308-915e-4d80-9f1d-5cd59a06bc1a"
 }
 
 let producto33={
     nombre:"Forza Horizon 2",
-    precio:500000,
+    precio:52500,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ForzaHorizon2.jpg?alt=media&token=5fe0d4b4-85d5-44c3-ac4e-a48f5cc6f24d"
 }
 
 let producto34={
     nombre:"Forza Horizon 3",
-    precio:500000,
+    precio:86700,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/forzahorizon3.jpg?alt=media&token=49a235be-4894-4049-aa9b-f3552b231867"
 }
 
 let producto35={
     nombre:"Forza Horizon 4",
-    precio:500000,
+    precio:136700,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ForzaHorizon4.jpg?alt=media&token=cff918d3-e46d-4e71-a290-128fbf1c58fd"
 }
 let producto36={
     nombre:"Forza Horizon 5",
-    precio:500000,
+    precio:195900,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ForzaHorizon5.jpg?alt=media&token=be57df00-5c23-486a-ba1f-4a509d18fc50"
 }
 let producto37={
     nombre:"Far Cry 3",
-    precio:500000,
+    precio:33000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/FarCry3.jpg?alt=media&token=68acfaab-6bd0-453c-8527-8c3d310f7097"
 }
 let producto38={
     nombre:"Far Cry 4",
-    precio:500000,
+    precio:53400,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/FarCry4.jpg?alt=media&token=636f6f88-964e-4118-9e70-d074fed607ba"
 }
 let producto39={
     nombre:"Far Cry 5",
-    precio:500000,
+    precio:93500,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★☆☆☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/FarCry5.jpg?alt=media&token=1e40e6e2-3db5-4fa0-be6e-08241beaa3b5"
 }
 let producto40={
     nombre:"Far Cry 6",
-    precio:500000,
+    precio:147900,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/FarCry6.jpg?alt=media&token=ed6032fe-6a3d-4ee4-bf04-35a3cfd9136f"
 }
 let producto41={
     nombre:"Borderlands",
-    precio:500000,
+    precio:43600,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Borderlands.jpg?alt=media&token=8a2d999d-742c-4a8c-aecf-2bddb808d1c0"
 }
 
 let producto42={
     nombre:"Borderlands 2",
-    precio:500000,
+    precio:67800,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Borderlands2.jpg?alt=media&token=970d9314-476b-4a6f-99f7-3816a376711c"
 }
 
 let producto43={
     nombre:"Borderlands The Pre-Sequel",
-    precio:500000,
+    precio:72000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BorderlansPreSequel.jpg?alt=media&token=71ad9622-d538-4af4-b8ae-49160313c9c3"
 }
 
 let producto44={
     nombre:"Borderlands  3",
-    precio:500000,
+    precio:135000,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/Borderlands3.jpg?alt=media&token=6c5e9de1-859d-40e1-b66d-6086aa2997c0"
 }
 
 let producto45={
     nombre:"Assassin's Creed Origins",
-    precio:500000,
+    precio:94000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ACOrigins.jpg?alt=media&token=d4942c29-fd28-42c6-a4d4-011e921250e3"
 }
 let producto46={
     nombre:"Assassin's Creed Odyssey",
-    precio:500000,
+    precio:124000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ACOdyssey.jpg?alt=media&token=d0b35d72-ef02-402e-9654-49a852fe51c8"
 }
 let producto47={
     nombre:"Assasins's Creed Valhalla",
-    precio:500000,
+    precio:167000,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Francia",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/ACValhalla.jpg?alt=media&token=8c8ae01c-939f-4744-a769-f1c26a60ffa1"
 }
 let producto48={
     nombre:"Battlefield 4",
-    precio:500000,
+    precio:40000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★★☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BTF4.jpg?alt=media&token=333e8118-6d18-482e-90ef-77dd914d82bf"
 }
 let producto49={
     nombre:"Battlefield 1",
-    precio:500000,
+    precio:59000,
     estado:"Nuevo",
     popularidad:"★★★★★",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BTF1.jpg?alt=media&token=b8915c76-a7bf-4a42-ba73-f304315350ae"
 }
 let producto50={
     nombre:"Battlefield 5",
-    precio:500000,
+    precio:870000,
     estado:"Nuevo",
-    popularidad:"★★★★★",
+    popularidad:"★★★☆☆",
+    pais:"Estados Unidos",
     foto:"https://firebasestorage.googleapis.com/v0/b/tienda-3a0de.appspot.com/o/BTF5.jpg?alt=media&token=aabc0031-9985-4de1-9621-c3c8eddf634c"
 }
 //ARREGLO
@@ -459,12 +509,17 @@ productos.forEach(function(producto){
     popularidadProducto.classList.add("text-center")
     popularidadProducto.textContent="Popularidad:"+" "+producto.popularidad
 
+    let paisProducto=document.createElement("p")
+    paisProducto.classList.add("text-center")
+    paisProducto.textContent="País:"+" "+producto.pais
+
     
     tarjeta.appendChild(foto)
     tarjeta.appendChild(nombreProducto)
     tarjeta.appendChild(precioProducto)
     tarjeta.appendChild(estadoProducto)
     tarjeta.appendChild(popularidadProducto)
+    tarjeta.appendChild(paisProducto)
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
 
